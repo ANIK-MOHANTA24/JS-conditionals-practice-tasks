62c0d86c889b666dc24bfb2fec4fd04aaed9627b
ANIK-MOHANTA24 <mohontoa050@gmail.com>
2024-01-31 00:04:56 +0600

    added nested condition

--- 4_nested-friend.js
+++ 4_nested-friend.js
@@ -10,3 +10,27 @@ if you get less than 80 go to home and sleep and act sad
 Note: 
 use nested if-else-if-else
 */
+
+const myScore = 80;
+const friendScore = 39;
+
+if (myScore >= 80) {
+    if (friendScore >= 80) {
+        console.log("Let's go for a lunch.")
+    }
+    else if (friendScore < 80 && friendScore >= 60) {
+        console.log("good luck next time. ")
+    }
+    else if (friendScore < 60 && friendScore >= 40) {
+        console.log("keep your friend's message unseen. ")
+    }
+    // else if (friendScore < 40) {
+    //     console.log("block your friend.")
+    // }
+    else {
+        console.log("block your friend. ")
+    }
+}
+else {
+    console.log("go to home and sleep and act sad")
+}
